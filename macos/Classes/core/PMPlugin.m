fa7dec6f39ddad9f65c9e6975d9b5bab4fe7994a
Ashot <ashot.jlavyan@gmail.com>
2020-08-05 17:54:50 +0400

    FIX crash on create album

--- macos/Classes/core/PMPlugin.m
+++ macos/Classes/core/PMPlugin.m
@@ -275,7 +275,7 @@
         }
 
         [manager createFolderWithName:name parentId:parentId block:^(NSString *id, NSString *errorMsg) {
-            [handler reply:[self convertToResult:@{@"id": id, @"errorMsg": errorMsg}]];
+            [handler reply:[self convertToResult:id errorMsg:errorMsg]];
         }];
 
       } else if ([@"createAlbum" isEqualToString:call.method]) {
@@ -288,16 +288,14 @@
         }
 
         [manager createAlbumWithName:name parentId:parentId block:^(NSString *id, NSString *errorMsg) {
-            NSDictionary *dictionary = @{@"id": id, @"errorMsg": errorMsg};
-            [handler reply:[self convertToResult:dictionary]];
+            [handler reply:[self convertToResult:id errorMsg:errorMsg]];
         }];
 
       } else if ([@"getAlbum" isEqualToString:call.method]) {
         NSString *name = call.arguments[@"name"];
 
         [manager getAlbumWithName:name block:^(NSString *id, NSString *errorMsg) {
-            NSDictionary *dictionary = @{@"id": id, @"errorMsg": errorMsg};
-            [handler reply:[self convertToResult:dictionary]];
+            [handler reply:[self convertToResult:id errorMsg:errorMsg]];
         }];
 
       } else if ([@"removeInAlbum" isEqualToString:call.method]) {
@@ -335,17 +333,17 @@
 
 }
 
-- (NSDictionary *)convertToResult:(NSDictionary *)dict {
-  NSMutableDictionary *result = [NSMutableDictionary new];
-
-  for (id key in dict.allKeys) {
-    id value = dict[key];
-    if (value) {
-      result[key] = value;
+- (NSDictionary *)convertToResult:(NSString *)id errorMsg:(NSString *)errorMsg {
+    NSMutableDictionary *mutableDictionary = [NSMutableDictionary new];
+    if (errorMsg) {
+        mutableDictionary[@"errorMsg"] = errorMsg;
     }
-  }
 
-  return result;
+    if (id) {
+        mutableDictionary[@"id"] = id;
+    }
+
+    return mutableDictionary;
 }
 
 @end
